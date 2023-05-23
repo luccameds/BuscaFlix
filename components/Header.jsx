61e2561296ea1f1669a0a3fcee3e0210f3466d2b
Lucca Medeiros <luccameds@gmail.com>
2023-05-23 20:12:07 -0300

    Complete the Header.jsx in components

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export const Header = () => {
-  return <div className="mx-10 my-10 bg-black">Header</div>;
+  return (
+    <div className="px-10 py-10 bg-[#121829] text-white flex flex-row justify-between items-center">
+      <img src="./logo.svg" alt="logo" />
+      <div className="flex flex-row text-[#A8AEBF]">
+        <p className="mx-[15px] font-bold">Movies</p>
+        <p className="mx-[15px] font-bold ">TV Shows</p>
+        <div className="flex flex-row">
+          <p className="mx-[15px] font-bold">Suggest me</p>
+          <img
+            src="./arrow-right.svg"
+            alt="arrow-right"
+            className="w-[20px] h-[20px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };

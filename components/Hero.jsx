852ed26dbf339a19f5fa2e2591684b1d16346622
Lucca Medeiros <luccameds@gmail.com>
2023-05-24 22:16:43 -0300

    Created the Hero in components

---
+++ components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Cards from './Cards';
+
+const Hero = () => {
+  return (
+    <div className="bg-[#121829] py-[40px]">
+      <div className="mx-10">
+        <p className="font-light">MovieMaze</p>
+        <h1 className="text-[50px] font-bold">Filmes</h1>
+        <div className="my-[30px] flex h-[60px] w-[300px] items-center justify-between rounded-xl border-[1px] border-[#475069] px-[30px] backdrop:bg-[#32467e]">
+          <img src="./search.svg" alt="search" className="h-[30px] w-[30px]" />
+          <p className="text-[14px] text-[#475069]">
+            Procurar Filmes ou TV Shows
+          </p>
+        </div>
+        <div className="my-[30px] text-[18px] text-[#767E94]">
+          <p>120 Itens</p>
+        </div>
+        <Cards />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;

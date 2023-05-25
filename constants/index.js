bc9686e40421299e70c17605394e455930c2cffc
Lucca Medeiros <luccameds@gmail.com>
2023-05-25 19:47:51 -0300

    Created dinamic search images in constants

---
+++ constants/index.js
@@ -0,0 +1,32 @@
+export const exploreposters = [
+  {
+    id: 'poster-1',
+    imgUrl: '/poster-01.svg',
+    title: 'Two And a Half Men',
+    rating: '6,9',
+  },
+  {
+    id: 'poster-2',
+    imgUrl: '/poster-02.svg',
+    title: 'The Big Bang Theory',
+    rating: '9,9',
+  },
+  {
+    id: 'poster-3',
+    imgUrl: '/poster-03.svg',
+    title: 'Friends',
+    rating: '4,5',
+  },
+  {
+    id: 'poster-4',
+    imgUrl: '/poster-04.svg',
+    title: 'Money Heist',
+    rating: '6,9',
+  },
+  {
+    id: 'poster-5',
+    imgUrl: '/poster-05.svg',
+    title: 'Black Widow',
+    rating: '8,9',
+  },
+];
